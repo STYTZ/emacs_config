ec66caae68ca67dd02441dc6d2f58496146084c5
Алексей Стыценко <astyts@yandex-team.ru>
2016-10-18 23:40:35 +0300

    Add solarized theme

--- init.el
+++ init.el
@@ -1,2 +1,6 @@
 (add-to-list 'load-path "~/.emacs.d/plugins/better-defaults")
 (require 'better-defaults)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
+(load-theme 'solarized t)
+(custom-set-variables '(frame-background-mode (quote dark)))
