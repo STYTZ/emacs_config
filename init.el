6573175b627490fc5f7b55bb40758f13fb482ef4
Алексей Стыценко <astyts@yandex-team.ru>
2016-10-22 00:26:57 +0300

    Add 'el-get' with 'elpy' and some other stuff

--- init.el
+++ init.el
@@ -1,6 +1,41 @@
-(add-to-list 'load-path "~/.emacs.d/plugins/better-defaults")
-(require 'better-defaults)
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+;; (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
+
+(setq my-el-get-packages
+      '(
+        better-defaults
+        color-theme-solarized
+        elpy
+        ;; magit
+        markdown-mode
+        yaml-mode
+    ))
+(el-get 'sync my-el-get-packages)
+
+(elpy-enable)
+
 (load-theme 'solarized t)
-(custom-set-variables '(frame-background-mode (quote dark)))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elpy-modules (quote (elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-pyvenv elpy-module-yasnippet elpy-module-sane-defaults)))
+ '(elpy-rpc-backend "jedi")
+ '(frame-background-mode (quote dark)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
